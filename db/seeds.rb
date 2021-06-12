95336877eda7e6d921450a6a132b5a9681112632
Netaly <netaly.ramirez@gmail.com>
2021-06-12 13:26:47 -0400

    added a test seed

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+
+luna = User.create(username: "LunaLovegood")
+
+quibbler = ReadingList.create(name: "Research List", user_id: luna.id)
+
+hp1 = Book.create(title: "Socercers Stone", author: "J.K Rowling", publishing_company: "millers")
+
+BookRoster.create(book: hp1, reading_list: quibbler)
+
+puts "Done Seeding"
