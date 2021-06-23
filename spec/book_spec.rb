2e1a1d8695a5735de6f3f4d103e12c3b8c6d6330
Netaly <netaly.ramirez@gmail.com>
2021-06-23 19:55:17 -0400

    created tests for book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,26 @@
 # class methods are prefixed with a dot (".add"), 
 # instance methods with a dash ("#add").
 require "spec_helper"
-require 'active_record'
 
 RSpec.describe Book do 
-    
+    test_search_result = [
+        {:value=>{:title=>"Harry Potter Coloring Book", :author=>"Inc. Scholastic", :publisher=>"Scholastic Incorporated"}, :name=>"  📖 \"Harry Potter Coloring Book\"\n   Author: Inc. Scholastic\n    Publisher: Scholastic Incorporated"}, {:value=>{:title=>"Fantastic Beasts and Where to Find Them: Cinematic Guide: Newt Scamander Do Not Feed Out", :author=>"Felicity Baker", :publisher=>"N/A"}, :name=>"  📖 \"Fantastic Beasts and Where to Find Them: Cinematic Guide: Newt Scamander Do Not Feed Out\"\n   Author: Felicity Baker\n    Publisher: "}, {:value=>{:title=>"Harry Potter: The Complete Collection (1-7)", :author=>"J.K. Rowling", :publisher=>"Pottermore Publishing"}, :name=>"  📖 \"Harry Potter: The Complete Collection (1-7)\"\n   Author: J.K. Rowling\n    Publisher: Pottermore Publishing"}, {:value=>{:title=>"Harry Potter and Philosophy", :author=>"David Baggett", :publisher=>"Open Court Publishing"}, :name=>"  📖 \"Harry Potter and Philosophy\"\n   Author: David Baggett\n    Publisher: Open Court Publishing"}, {:value=>{:title=>"Harry Potter and the Half-Blood Prince", :author=>"J. K. Rowling", :publisher=>"N/A"}, :name=>"  📖 \"Harry Potter and the Half-Blood Prince\"\n   Author: J. K. Rowling\n    Publisher: "}
+    ]
+    test_book = {"title" => "Harry Potter Coloring Book", "authors" => ["Inc. Scholastic"], "publisher"=> "Scholastic Incorporated"}
+
+
     describe ".get_books" do 
         context "Given a query" do
             it "should return an array of objects" do
-                expect(Book.get_books("harry potter")).to(true)
+                expect(Book.get_books("harry potter").length).to eq(5)
+            end
+        end
+    end
+
+    describe ".format_book_info" do
+        context "given a book" do
+            it "should format the books info" do
+                expect(Book.format_book_info(test_book)).not_to be_empty
             end
         end
     end
---
+++ spec/user_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe User do
+    describe "#get_reading_list_choices" do
+        puts "activated"
+    end
+end
